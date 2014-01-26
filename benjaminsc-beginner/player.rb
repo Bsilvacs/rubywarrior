d6792cdc4cb757f43af6529ead1342eff1c6fd95
BenjaminSilva <mascara_sagrada@hotmail.com>
2014-01-26 13:39:22 -0600

    Up to level 7 for now.

--- benjaminsc-beginner/player.rb
+++ benjaminsc-beginner/player.rb
@@ -1,5 +1,47 @@
 class Player
   def play_turn(warrior)
-    # add your code here
+  	if @already_gone_backwards || warrior.feel(:backward).wall?
+  		@already_gone_backwards = true
+	  	if warrior.feel.empty?
+	  		walk_or_rest(warrior, :forward)
+	  	else
+	  		warrior_or_captive(warrior, :forward)
+	  	end
+  	else
+	  	if warrior.feel(:backward).empty?
+	  		walk_or_rest(warrior, :backward)
+	  	else
+	  		warrior_or_captive(warrior, :backward)
+	  	end
+  	end
+  	@health = warrior.health
+  end
+
+  def walk_or_rest(warrior, direction)
+  	if @health.nil? || @health > warrior.health
+  		if warrior.health < 10
+  			if(direction == :forward)
+  				warrior.walk!(:backward)
+			else
+  				warrior.walk!(:forward)
+			end
+  		else
+  			warrior.walk!(direction)
+  		end
+  	else
+	  	if warrior.health < 20 && !warrior.feel(direction).stairs?
+  			warrior.rest!
+  		else
+  			warrior.walk!(direction)
+		end	
+  	end
+  end
+
+  def warrior_or_captive(warrior, direction)
+  	if warrior.feel(direction).captive?
+  		warrior.rescue!(direction)
+  	else
+  		warrior.attack!(direction)
+  	end
   end
 end
